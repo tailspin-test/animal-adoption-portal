9de48f3fbb7edf22746faaa217f644e5dffa2547
Jacob Cheatley (AWARE) <jacob.cheatley@awaregroup.com>
2021-02-17 09:29:39 +1300

    Update chrome driver test options

--- AnimalAdoption.Web.Portal.FunctionalTests/ChromeDriverTest.cs
+++ AnimalAdoption.Web.Portal.FunctionalTests/ChromeDriverTest.cs
@@ -24,6 +24,7 @@ namespace AnimalAdoption.Web.Portal.FunctionalTests
             {
                 PageLoadStrategy = PageLoadStrategy.Normal
             };
+            options.AddArguments("--no-sandbox", "--headless");
             _driver = new ChromeDriver(options);
         }
 
